af1b023e8a1be76ecc75559747fcd66e052844d1
Yatender <ok@yimac.local>
2019-01-07 12:10:07 +0530

    setting up test cases

---
+++ test/urlner_web/controllers/link_test.exs
@@ -0,0 +1,31 @@
+defmodule Urlner.Link.Test do
+  use ExUnit.case
+    alias Urlner.Link.{
+      Model
+    }
+
+    test "generate short url" do
+
+    end
+
+    test "generate short url with params" do
+
+    end
+
+    test "get original url" do
+
+    end
+
+    test "get original url having params" do
+
+    end
+
+    test "get original url having params from short url passing token in short url" do
+
+    end
+
+    test "invalid short url passing" do
+
+    end
+  end
+end
