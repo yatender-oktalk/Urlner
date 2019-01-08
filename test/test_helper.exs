79ea41935e16bdc845b22a7900e042c14525198a
Yatender <ok@yimac.local>
2019-01-08 11:02:05 +0530

    removing unfinished tests (no needed here but still adding for future)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,4 +4,4 @@ Mix.Task.run "ecto.create", ~w(-r Urlner.Repo --quiet)
 Mix.Task.run "ecto.migrate", ~w(-r Urlner.Repo --quiet)
 
 Ecto.Adapters.SQL.Sandbox.mode(Urlner.Repo, {:shared, self()})
-
+ExUnit.start(exclude: [:pending])
